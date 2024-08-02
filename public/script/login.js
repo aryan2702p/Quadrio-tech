d4375a8ae0e5ec9e0a158f30466e39ec5fe5143b
aryan2702p <arynmi02@gmail.com>
2024-08-02 21:27:51 +0530

    updated

--- public/script/login.js
+++ public/script/login.js
@@ -1,7 +1,7 @@
 async function login_user(data) {
     console.log("userlogin function called");
     try {
-        const response = await fetch('http://localhost:3000/api/auth/login', {
+        const response = await fetch('/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
